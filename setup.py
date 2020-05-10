5f183565168ba18edca2b982d09354e76c2e6fa0
QuantumCalzone <QuantumCalzone@gmail.com>
2020-05-09 22:41:51 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonwebscraper',
+    version='0.1',
+    packages=['pythonwebscraper'],
+    url='https://github.com/QuantumCalzone/pythonwebscraper',
+    license='',
+    author='QuantumCalzone',
+    author_email='QuantumCalzone@gmail.com',
+    description=''
+)
